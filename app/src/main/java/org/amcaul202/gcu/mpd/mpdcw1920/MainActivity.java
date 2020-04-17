b7da742362447b22e30ee2c5aaaaf6f446a5d844
amcaul202 <47104222+amcaul202@users.noreply.github.com>
2020-04-17 20:13:42 +0100

    Commit #1
    Use of listView (Not Fully Implemented)
     - Works with single defined link

--- app/src/main/java/org/amcaul202/gcu/mpd/mpdcw1920/MainActivity.java
+++ app/src/main/java/org/amcaul202/gcu/mpd/mpdcw1920/MainActivity.java
@@ -1,8 +1,11 @@
 package org.amcaul202.gcu.mpd.mpdcw1920;
 
+import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,13 +17,17 @@ import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {//implements View.OnClickListener{
     private TextView rawDataDisplay;
     private String result;
 
@@ -172,5 +179,129 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    /*List headlines;
+    //List links;
+    //List descs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //new Task().execute();
+
+        RwButton = (Button) findViewById(R.id.RwButton);
+        PlanRwButton = (Button) findViewById(R.id.PlanRwButton);
+        CurrentInButton = (Button) findViewById(R.id.CurrentInButton);
+        RwButton.setOnClickListener(this);
+        PlanRwButton.setOnClickListener(this);
+        CurrentInButton.setOnClickListener(this);
+    }
+
+    public void onClick(View aview) {
+        //IF Statement for XML Links
+        if (aview != null) {
+            int id = aview.getId();
+
+            if (id == R.id.RwButton) {
+                String urlSource = urlSource1;
+                startProgress(urlSource);
+            } else if (id == R.id.PlanRwButton) {
+                String urlSource = urlSource2;
+                startProgress(urlSource);
+            } else if (id == R.id.CurrentInButton) {
+                String urlSource = urlSource3;
+                startProgress(urlSource);
+            }
+        }
+    }
+
+    public void startProgress(String urlSource) {
+        new Task(urlSource).execute();
+    }
+
+    class Task extends AsyncTask<Object, Void, ArrayAdapter> {
+        public String url;
+
+        public Task(String aurl) {
+            url = aurl;
+        }
+
+        @Override
+        protected  ArrayAdapter doInBackground(Object[] params) {
+            headlines = new ArrayList();
+            //links = new ArrayList();
+            //descs = new ArrayList();
+            try {
+                URL aurl = null;
+                URLConnection yc;
+                BufferedReader in = null;
+                String inputLine = "";
+
+                Log.e("MyTag", "in run"); //Tag for Testing (REMOVE)
+
+                try {
+                    Log.e("MyTag", "in try"); //Tag for Testing (REMOVE)
+                    aurl = new URL(url);
+                    yc = aurl.openConnection();
+                    in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+                    result = in.readLine();
+                    while ((inputLine = in.readLine()) != null) {
+                        result = result + inputLine;
+                        Log.e("MyTag", inputLine);
+
+                    }
+                    in.close();
+                } catch (IOException ae) {
+                    Log.e("MyTag", "ioexception");
+                }
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(false);
+                XmlPullParser xpp = factory.newPullParser();
+
+                xpp.setInput(getInputStream(aurl), "UTF_8");
+                boolean insideItem = false;
+
+                int eventType = xpp.getEventType();
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    if (eventType == XmlPullParser.START_TAG) {
+                        if (xpp.getName().equalsIgnoreCase("item")){
+                            insideItem = true;
+                        } else if (xpp.getName().equalsIgnoreCase("title")) {
+                            if (insideItem)
+                                headlines.add(xpp.nextText());
+                        } else if (xpp.getName().equalsIgnoreCase("link")) {
+                            if (insideItem)
+                                headlines.add(xpp.nextText());
+                        } else if (xpp.getName().equalsIgnoreCase("description")) {
+                            if (insideItem)
+                                headlines.add(xpp.nextText());
+                        }
+                    } else if (eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item")) {
+                        insideItem = false;
+                    }
+                    eventType = xpp.next();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } return null;
+        }
+
+        protected void onPostExecute(ArrayAdapter adapter) {
+            adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, headlines);
+            //adapter2 = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_2, links);
+            setListAdapter(adapter);
+        }
+
+        public InputStream getInputStream(URL url){
+            try {
+                return url.openConnection().getInputStream();
+            } catch (IOException e){
+                return null;
+            }
+        }
+    }*/
 
 } // End of MainActivity
